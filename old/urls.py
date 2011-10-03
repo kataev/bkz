b963d5f43f2295fa598d2b9d170903971f6c927d
Катаев Денис <bteamko+git@gmail.com>
2011-10-03 16:48:23 +0600

    Приложение для конвертации старой базы

--- old/urls.py
+++ old/urls.py
@@ -1,3 +1,7 @@
 from django.conf.urls.defaults import *
 
-# place app url patterns here
+urlpatterns = patterns('whs.old.views',
+    url(r'^old/brick/$', 'fetch_brick',name='fetch_brick'),
+    url(r'^old/opers/$', 'fetch_oper',name='fetch_oper'),
+    url(r'^old/total/$', 'fetch_total',name='fetch_total'),
+    )
--- old/views.py
+++ old/views.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
-from old.models import *
-#from brick.models import *
+from whs.old.models import Agent as Aold
+from whs.brick.models import OldBrick
+from whs.old.models import Tovar,Jurnal,Sclad
+from whs.agent.models import Agent
+from whs.bill.models import *
 import re
+from django.http import HttpResponse
 
 view_c=((u'Л',u'Лицевой'),(u'Р',u'Рядовой'))
 weight_c=((u'1',u'Одинарный'),(u'1.4',u'Утолщенный'),(u'2',u'Двойной'))
@@ -32,21 +36,79 @@ type_c=(('',''),('1','1 тип'),('2','2 тип'),('3','3 тип'))
 defect_c=((u'',u''),(u'<20',u'До 20%'),(u'>20',u'Более 20%'))
 refuse_c=((u'',u''),(u'Ф',u'Фаска'),(u'ФП',u'Фаска Полосы'),(u'ФФ',u'Фаска Фаска'),(u'ФФП',u'Фаска Фаска Полосы'),(u'П',u'Полосы'))
 
-
-def fetch_brick():
+def fetch_brick(request):
     tovar = Tovar.objects.using('old').all().order_by('id')
-#    bricks = map(lambda x: x[0],Brick.objects.values_list('pk').order_by('id'))
+    responce = u''
     for b in tovar:
         prim = b.prim
         mark =  re.search('100|125|150|175|200 |250',prim)
         if mark: mark = int(mark.group())
         else: mark = 9000
-        cl = re.search('КЕ',prim)
+#        print prim
+        cl = re.search(u'КЕ',prim,re.U)
         if cl:
-            cl = 6
+            cl = 5
+            if re.search(u'УЛ',prim,re.U):
+                view = u'Л'
+            else:
+                view = u'Р'
+            weight = u'1'
         else:
-            cl = re.search('Желтый|Коричневый',prim)
+            cl = re.search(u'желтый|корич|светлый|белый',prim,re.U)
             if cl:
                 cl = cl.group()
-        print cl
+                for c in class_c:
+                    if unicode(cl) in c[1].lower():
+                        cl = c[0]
+            else:
+                cl = 0
+            if re.search(u'ЛПу',prim,re.U):
+                view = u'Л'
+            else:
+                view = u'Р'
+            if re.search(u'КО',prim,re.U):
+                weight = u'1'
+            else:
+                weight = u'1.4'
+        responce+='<div>%s | %s | %s | %s</div>' % (b.prim,cl,class_c[cl][1],view)
+        try: OldBrick.objects.get(old_id=b.id)
+        except OldBrick.DoesNotExist:
+            bri = OldBrick(mark=mark,brick_class=cl,color=u'Кр',view=view,weight=weight,name=b.prim,css='1',label='1',old_id=b.id)
+            bri.full_clean()
+            bri.save()
+    return HttpResponse(responce)
 
+def fetch_total(request):
+    for s in  Sclad.objects.using('old').all().order_by('id'):
+        try: br = OldBrick.objects.get(old_id=s.id)
+        except OldBrick.DoesNotExist: pass
+        if br:
+            br.total = s.total
+            br.save()
+    return HttpResponse()
+
+def agents():
+    agent = Aold.objects.using('old').all().order_by('id')
+    for a in agent:
+        Agent(pk=a.id,name=a.name).save()
+
+def fetch_oper(request):
+    responce = u''
+    i = 99990
+    for j in Jurnal.objects.using('old').filter(pk__gt=2000).order_by('id'):
+        if j.minus:
+            try:nakl = int(j.nakl)
+            except ValueError:
+                    nakl = i
+                    i+=1
+                    print j.nakl,j.pk
+            try: bill = Bill.objects.get(number=nakl,date=j.date)
+            except Bill.DoesNotExist:
+                bill = Bill(number=nakl,date=j.date,agent=Agent.objects.get(pk=j.agent),info=j.prim)
+                bill.save()
+            s = Sold(brick=OldBrick.objects.get(old_id=j.tov),
+                     amount=j.minus,doc=bill,price=j.price,delivery=j.trans)
+            s.save()
+
+
+    return HttpResponse(responce)
