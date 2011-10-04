6202fca80850b8a259b224c0c83877715c9a6f3f
Катаев Денис <bteamko+git@gmail.com>
2011-10-04 10:07:11 +0600

    Прилоежние для выгрузки перевода

--- old/urls.py
+++ old/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('whs.old.views',
     url(r'^old/brick/$', 'fetch_brick',name='fetch_brick'),
-    url(r'^old/opers/$', 'fetch_oper',name='fetch_oper'),
+    url(r'^old/sold/$', 'fetch_oper',name='fetch_oper'),
+    url(r'^old/tran/$', 'fetch_transfer',name='fetch_transfer'),
     url(r'^old/total/$', 'fetch_total',name='fetch_total'),
     )
--- old/views.py
+++ old/views.py
@@ -6,6 +6,7 @@ from whs.agent.models import Agent
 from whs.bill.models import *
 import re
 from django.http import HttpResponse
+import datetime
 
 view_c=((u'Л',u'Лицевой'),(u'Р',u'Рядовой'))
 weight_c=((u'1',u'Одинарный'),(u'1.4',u'Утолщенный'),(u'2',u'Двойной'))
@@ -31,12 +32,10 @@ mark_c=((100,u'100'),
         (250,u'250'),
         (9000,u'брак'))
 
-
-type_c=(('',''),('1','1 тип'),('2','2 тип'),('3','3 тип'))
-defect_c=((u'',u''),(u'<20',u'До 20%'),(u'>20',u'Более 20%'))
-refuse_c=((u'',u''),(u'Ф',u'Фаска'),(u'ФП',u'Фаска Полосы'),(u'ФФ',u'Фаска Фаска'),(u'ФФП',u'Фаска Фаска Полосы'),(u'П',u'Полосы'))
-
 def fetch_brick(request):
+    """
+    Доставание кирпича, просто мрак!
+    """
     tovar = Tovar.objects.using('old').all().order_by('id')
     responce = u''
     for b in tovar:
@@ -79,36 +78,64 @@ def fetch_brick(request):
     return HttpResponse(responce)
 
 def fetch_total(request):
-    for s in  Sclad.objects.using('old').all().order_by('id'):
-        try: br = OldBrick.objects.get(old_id=s.id)
-        except OldBrick.DoesNotExist: pass
-        if br:
-            br.total = s.total
-            br.save()
-    return HttpResponse()
+    """
+    Актуализировать остатки кирпичей.
+    """
+    delete = ''
+    for b in OldBrick.objects.all():
+        try: b.total = Sclad.objects.using('old').get(pk=b.old_id).total
+        except Sclad.DoesNotExist:
+            print b.pk,b.old_id
+            if not Jurnal.objects.using('old').filter(tov=b.old_id).count():
+                delete+=' deleted %d' % b.pk
+                b.delete()
+        b.save()
+    return HttpResponse('FINISH %s' %delete)
 
 def agents():
+    """
+    Перенос агентов
+    """
     agent = Aold.objects.using('old').all().order_by('id')
     for a in agent:
         Agent(pk=a.id,name=a.name).save()
+    return HttpResponse('FINISH')
+
+def fetch_transfer(request):
+    """
+    Перенос переводов с угадыванием точки получения
+    """
+    a = {}
+    for j in Jurnal.objects.using('old').filter(pakt__gt=0):
+        if j.date:
+            s = Sold.objects.filter(doc__date__month=j.date.month, doc__date__year=j.date.year,
+                                    amount__gte=j.pakt,brick=OldBrick.objects.get(old_id=j.tov))
+            if s:
+                a[j.akt] = s[0]
+    print 'finish sold',a
+    for j in Jurnal.objects.using('old').filter(makt__gt=0):
+        if a.get(j.akt):
+            t = Transfer(brick=OldBrick.objects.get(old_id=j.tov),
+                         amount=j.makt,sold=a[j.akt],doc=a[j.akt].doc)
+            t.save()
+    return HttpResponse('FINISH')
 
 def fetch_oper(request):
-    responce = u''
+    """
+    Достать продажи из старой базы и положить в новую.
+    """
     i = 99990
-    for j in Jurnal.objects.using('old').filter(pk__gt=2000).order_by('id'):
-        if j.minus:
-            try:nakl = int(j.nakl)
-            except ValueError:
-                    nakl = i
-                    i+=1
-                    print j.nakl,j.pk
-            try: bill = Bill.objects.get(number=nakl,date=j.date)
-            except Bill.DoesNotExist:
-                bill = Bill(number=nakl,date=j.date,agent=Agent.objects.get(pk=j.agent),info=j.prim)
-                bill.save()
-            s = Sold(brick=OldBrick.objects.get(old_id=j.tov),
-                     amount=j.minus,doc=bill,price=j.price,delivery=j.trans)
-            s.save()
-
-
-    return HttpResponse(responce)
+    for j in Jurnal.objects.using('old').filter(minus__gt=0).order_by('id'):
+        try:nakl = int(j.nakl)
+        except ValueError:
+                nakl = i
+                i+=1
+                print j.nakl,j.pk
+        try: bill = Bill.objects.get(number=nakl,date=j.date)
+        except Bill.DoesNotExist:
+            bill = Bill(number=nakl,date=j.date,agent=Agent.objects.get(pk=j.agent),info=j.prim)
+            bill.save()
+        s = Sold(brick=OldBrick.objects.get(old_id=j.tov),
+                 amount=j.minus,doc=bill,price=j.price,delivery=j.trans)
+        s.save()
+    return HttpResponse('FINISH')
