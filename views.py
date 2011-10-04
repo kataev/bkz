96a6926e5aa6e59568539c20da1c7721e3271cd6
Катаев Денис <bteamko+git@gmail.com>
2011-10-04 11:35:39 +0600

    Переписал bricks store

--- views.py
+++ views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
 from django.db.models import Q,Sum,F
 from whs.bill.models import Bill,Sold,Transfer
-from whs.brick.models import Brick
+from whs.brick.models import Brick,BrickTable
 from whs.brick.forms import BrickFilterForm
 from whs.bill.forms import Bills
 from datetime import date
@@ -97,14 +97,17 @@ def bricks_store(request):
     Представление для dojo store сводной таблицы кирпича.
     """
     store = BricksStore()
-    bills = Bill.objects.filter(date__month=date.today().month).values_list('pk')
-    solds = Sold.objects.filter(doc__in=bills)
-    trans = Transfer.objects.filter(doc__in=bills)
+    sold = Sold.objects.filter(doc__date__month=date.today().month,doc__date__year=date.today().year).values('brick').annotate(s=Sum('amount'))
+    t_from = Transfer.objects.filter(doc__date__month=date.today().month,doc__date__year=date.today().year).values('brick').annotate(s=Sum('amount'))
+    t_to = Transfer.objects.filter(doc__date__month=date.today().month,doc__date__year=date.today().year).values('sold__brick').annotate(s=Sum('amount'))
+    sold = dict(map(lambda x: [x['brick'],x['s']],sold))
+    t_from = dict(map(lambda x: [x['brick'],x['s']],t_from))
+    t_to = dict(map(lambda x: [x['sold__brick'],x['s']],t_to))
     for b in store.Meta.objects:
-        b.sold = solds.filter(brick=b).aggregate(s=Sum('amount')).values()[0] or 0
+        b.sold = sold.get(b.pk,0)
         b.plus = 0
-        b.t_from = trans.filter(brick=b).aggregate(s=Sum('amount')).values()[0] or 0
-        b.t_to = trans.filter(sold__brick=b).aggregate(s=Sum('amount')).values()[0] or 0
+        b.t_from = t_from.get(b.pk,0)
+        b.t_to = t_to.get(b.pk,0)
         b.begin = b.total + b.sold + b.t_from - b.t_to
     return HttpResponse(store.to_json(), mimetype='application/json')
 
