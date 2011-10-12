8bf9b4dfe4fa708e9b083501aae8893d2ae6b511
Катаев Денис <bteamko@gmail.com>
2011-10-12 22:27:39 +0600

    Докстринг в производстве

--- manufacture/models.py
+++ manufacture/models.py
@@ -5,8 +5,10 @@ from django.contrib import admin
 
 
 class Man(Doc):
+    """Класс документа для учета прихода кирпича с производства"""
     class Meta():
             verbose_name = u"производство за день"
 
 class Add(Oper):
+    """Класс операций для документа"""
     doc = models.ForeignKey(Man,blank=False,related_name="%(app_label)s_%(class)s_related",null=False)
