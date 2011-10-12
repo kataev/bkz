a6fe75cecb3ac5c9b74616faee9003901481201e
Катаев Денис <bteamko@gmail.com>
2011-10-12 22:39:21 +0600

    Админ и __unicode__ для производства

---
+++ manufacture/admin.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+__author__ = 'bteam'
+
+from django.contrib import admin
+from whs.manufacture.models import *
+
+class ManAdmin(admin.ModelAdmin):
+    pass
+
+class AddAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Man,ManAdmin)
+admin.site.register(Add,AddAdmin)
--- manufacture/models.py
+++ manufacture/models.py
@@ -2,13 +2,32 @@
 from whs.bill.models import Oper,Doc
 from django.db import models
 from django.contrib import admin
+import pytils
 
 
 class Man(Doc):
     """Класс документа для учета прихода кирпича с производства"""
     class Meta():
             verbose_name = u"производство за день"
+            verbose_name_plural = u"производство за день"
+    def __unicode__(self):
+        if self.pk:
+            date = pytils.dt.ru_strftime(u"%d %B %Y", inflected=True, date=self.date)
+            return u'Принятие на склад от %s' % date
+        else:
+            return u'Новый приход'
+
 
 class Add(Oper):
     """Класс операций для документа"""
     doc = models.ForeignKey(Man,blank=False,related_name="%(app_label)s_%(class)s_related",null=False)
+    class Meta():
+            verbose_name = u"производство"
+            verbose_name_plural = u"производство"
+    def __unicode__(self):
+        if self.pk:
+            date = pytils.dt.ru_strftime(u"%d %B %Y", inflected=True, date=self.doc.date)
+            return u'Операция принятия на склад от %s' % date
+        else:
+            return u'Новая операция'
+
