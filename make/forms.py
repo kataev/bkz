0f1cfb93cf4920da36979ec3a38e8bdc2587e3f4
Катаев Денис <bteamko@gmail.com>
2013-01-10 14:21:58 +0600

    Extentions

--- make/forms.py
+++ make/forms.py
@@ -9,7 +9,7 @@ from bkz.whs.forms import DateInput
 class FormingForm(BootstrapMixin, forms.ModelForm):
     class Meta:
         model = Forming
-        widgets = {'date': DateInput}
+        widgets = {'date': DateInput,'source':forms.HiddenInput}
 
 class WarrenForm(BootstrapMixin, forms.ModelForm):
     class Meta:
@@ -25,5 +25,5 @@ class WarrenTTOForm(BootstrapMixin, forms.ModelForm):
 	class Meta:
 		model = Warren
 
-WarrenFactory = modelformset_factory(Warren,form=WarrenForm,extra=4,max_num=4)
-WarrenTTOFactory = inlineformset_factory(Warren, Warren, WarrenTTOForm, exclude=('date',) , extra=3, max_num=3,can_delete=False)
+WarrenFactory = modelformset_factory(Warren,form=WarrenForm,extra=4,max_num=6)
+WarrenTTOFactory = inlineformset_factory(Warren, Warren, WarrenTTOForm, exclude=('date',) , extra=6, max_num=6,can_delete=False)
