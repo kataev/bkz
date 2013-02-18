ba4a965e37416ee88e7192d4ef65975e4cd48fe4
Катаев Денис <bteamko@gmail.com>
2013-02-18 11:36:23 +0600

    url fix

--- energy/urls.py
+++ energy/urls.py
@@ -5,6 +5,4 @@ urlpatterns = patterns('bkz.energy.views',
     url(r'^$', 'index', name='index'),
     url(ur'^Энергоресурсы$', 'energy', name='energy'),
     url(ur'^Теплоэнергия$', 'teplo', name='teplo'),
-    url(r'csv','energy_csv'),
-
 ) + app_urlpatterns('energy')
