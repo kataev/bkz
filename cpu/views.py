2535b15ba1a7d684d4aef08a69eddf1fa0cb565d
Катаев Денис <bteamko@gmail.com>
2013-02-20 16:16:35 +0600

    target firing

--- cpu/views.py
+++ cpu/views.py
@@ -2,10 +2,10 @@
 from django.utils import simplejson
 
 from django.shortcuts import render
-from bkz.cpu.models import positions,line
+from bkz.cpu.models import positions, line, target
 
 def index(request):
     """ Главная страница """
-    firing = list({'field':k,'point':v,'position':positions.get(v,'')} for k,v in line.items())
+    firing = list({'field':k,'point':v,'position':positions.get(v,''),'target':target[v]} for k,v in line.items())
     firing.sort(key=lambda x: x['position'])
     return render(request, 'cpu/index.html', dict(firing = simplejson.dumps(firing)))
