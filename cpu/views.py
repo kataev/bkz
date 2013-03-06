a08399fe81bd3ed096c83ae32aa8e335adb62d38
Катаев Денис <bteamko@gmail.com>
2013-03-06 20:36:49 +0600

    Update views.py
    Fix bug

--- cpu/views.py
+++ cpu/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.utils import simplejson
-from operator import attrgetter
+from operator import itemgetter
 
 from django.shortcuts import render
 from bkz.cpu.models import positions, line, target
@@ -8,5 +8,5 @@ from bkz.cpu.models import positions, line, target
 def index(request):
     """ Главная страница """
     firing = list({'field':k,'point':v,'position':positions.get(v,''),'target':target[v]} for k,v in line.items())
-    firing.sort(key=attrgetter('position'))
+    firing.sort(key=itemgetter('position'))
     return render(request, 'cpu/index.html', dict(firing = simplejson.dumps(firing)))
