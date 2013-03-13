430fadf81c9d6b0db9b010720343f9c51fc14633
Катаев Денис <bteamko@gmail.com>
2013-03-13 12:39:32 +0600

    Fix warren

--- make/views.py
+++ make/views.py
@@ -72,8 +72,9 @@ def warren(request):
                 source = w
             w.source = source
             try:
-                w.forming.warren = w
-                w.forming.save()
+                if w.forming:
+                    w.forming.warren = w
+                    w.forming.save()
                 w.forming = Forming.objects.filter(date__lt=w.date - delay, date__gte=w.date - timedelta).filter(tts=w.tts).order_by('-date')[0]
             except IndexError:
                 pass
