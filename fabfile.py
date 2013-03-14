0870cbb2e97341cb7006e05e1685bfe156e968f9
Катаев Денис <bteamko@gmail.com>
2013-03-14 09:18:01 +0600

    Fabric

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from fabric.api import *
+import fabtools
 
-env = env
-env.hosts = ['localhost','server']
+env.roledefs['production'] = ['bkz@server']
 
 env.dev_branch = 'dev'
 env.master_branch = 'master'
@@ -22,3 +22,24 @@ def push():
 
 def dbstart():
     local('sudo service postgresql start')
+
+def production_env():
+	env.user = 'bkz'
+	env.python = '/home/bkz/env/bin/python'
+	env.project_root = '~/bkz'
+
+@roles('production')
+def restart():
+	sudo('supervisorctl restart bkz',shell=False)
+
+@roles('production')
+def deploy():
+	production_env()
+	with cd(env.project_root):
+		run('git fetch')
+		run('git merge origin/master')
+		with fabtools.python.virtualenv('/home/bkz/env'):
+			run('./manage.py collectstatic --noinput')
+
+	# sudo('supervisorctl restart bkz',shell=False)
+	
